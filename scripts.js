e0ebfff8aff24f27dd0bc77e4c856813d62156c4
Dani <bmlgadvertising@gmail.com>
2024-03-23 22:43:50 -0700

    COMPLETE| Finish adding scenarios for each possible outcome between player and computer

--- scripts.js
+++ scripts.js
@@ -25,4 +25,42 @@ computerChoice = getCpuChoice();
 //Allows player to enter their selection
 playerChoice = prompt("Enter your choice: Rock, paper, scissor").toLowerCase();
 
-console.log(playerChoice);
+function playGame(pcChoice, playChoice) {
+    let message;
+    let mLost = "You Lost!";
+    let mWin = "You Win!";
+    let mTie = "ITS A TIE!";
+    
+
+    if(pcChoice === "rock" && playChoice === "paper") {
+        message = mWin;
+
+    } else if (pcChoice === "rock" && playChoice === "scissor") {
+        message = mLost;
+
+    }   else if (pcChoice === "rock" && playChoice === "rock") {
+        message = mTie;
+
+    }   else if (pcChoice === "paper" && playChoice === "paper") {
+        message = mTie;
+
+    }   else if (pcChoice === "paper" && playChoice === "rock") {
+        message = mLost; 
+
+    }   else if (pcChoice === "paper" && playChoice === "scissor") {
+        message = mWin;
+
+    }   else if (pcChoice === "scissor" && playChoice === "rock") {
+        message = mWin;
+
+    }   else if (pcChoice === "scissor" && playChoice === "paper") {
+        message = mLost;
+        
+    }   else if (pcChoice === "scissor" && playChoice === "scissor") {
+        message = mTie;
+    }   
+
+    return message;
+}
+
+console.log(playGame(computerChoice, playerChoice));
